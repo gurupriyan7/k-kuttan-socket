189c3d5b6ba41c1c89a096cb5bef0e27909b6b5d
Gurupriyan j <gurupriyanj007@gmail.com>
2024-06-13 19:09:34 +0530

    update the port

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
