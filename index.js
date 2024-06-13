6c3ff61ecdf5ca8a268bf24b3a492a30635ba9f4
Gurupriyan j <gurupriyanj007@gmail.com>
2024-06-13 15:10:23 +0530

    update the script

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-  const io = require("socket.io")(6000, {
-      cors: {
-        origin: "http://localhost:3000",
-      },
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  // cors: {
+  //   origin: "http://localhost:3000"
+  // }
+});
+
+app.use(cors());
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  // Add new User
+  socket.on("new-user-add", (newUserId) => {
+    // If user is not added previously
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({ userId: newUserId, socketId: socket.id });
+      console.log("New User Connected", activeUsers);
+    }
+    console.log("no active users");
+    // Send all active users to new user
+    io.emit("get-users", activeUsers);
+  });
+
+  // Send message
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    const duplicates = activeUsers.filter((user) => user.userId === receiverId);
+    duplicates.forEach((user) => {
+      io.to(user.socketId).emit("receive-message", data);
     });
-    
-    let activeUsers = [];
-    
-    io.on("connection", (socket) => {
-      // add new User
-      socket.on("new-user-add", (newUserId) => {
-        // if user is not added previously
-        if (!activeUsers.some((user) => user.userId === newUserId)) {
-          activeUsers.push({ userId: newUserId, socketId: socket.id });
-          console.log("New User Connected", activeUsers);
-        }
-        // send all active users to new user
-        io.emit("get-users", activeUsers);
-      });
+  });
 
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    // Remove user from active users
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    // Send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
+});
 
-      //send message
+// Define a simple route
+app.get("/", (req, res) => {
+  res.status(200).json("Server is running");
+});
 
-      socket.on("send-message", (data)=>{
-          const {recieverId} = data;
-          const duplicates = activeUsers.filter(function(val) {
-            return recieverId.indexOf(val.userId) != -1;
-          });
-          console.log("dupli",duplicates);
-          duplicates.forEach(element => {
-            console.log(element.socketId);
-            io.to(element.socketId).emit("recieve-message", data)
-            console.log(data);
-          });
-      })
-
-    
-
-
-      socket.on("disconnect", () => {
-          // remove user from active users
-          activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-          console.log("User Disconnected", activeUsers);
-          // send all active users to all users
-          io.emit("get-users", activeUsers);
-        });
-  })
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
