bfe6566939a552bc89b8b12d484f86a30ffaa807
Gurupriyan j <gurupriyanj007@gmail.com>
2024-06-28 13:06:07 +0530

    update the cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "https://kambikuttapan.in"]
   }
 });
 
